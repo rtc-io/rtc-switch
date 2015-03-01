dc4a24d7d0028bc37859c89ec2d55fc9d91c5452
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-01 21:30:06 +1100

    added simple ws server example

---
+++ examples/ws-server.js
@@ -0,0 +1,15 @@
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({ port: 8080 });
+var board = require('../')();
+
+wss.on('connection', function connection(ws) {
+  var peer = board.connect();
+
+  ws.on('message', peer.process);
+  peer.on('data', function(data) {
+    if (ws.readyState === 1) {
+      console.log('OUT <== ' + data);
+      ws.send(data);
+    }
+  });
+});
