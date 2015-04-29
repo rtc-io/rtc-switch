e37d38429c9bc4f9c9eb5cd95434562f989683e1
Damon Oehlman <damon.oehlman@gmail.com>
2015-04-29 15:47:13 +1000

    Remove commented out lines

--- test/roominfo.js
+++ test/roominfo.js
@@ -72,25 +72,3 @@ test('0:announce (reannounce, should not increase membercont)', function(t) {
   members[0].on('data', handleData);
   members[0].process(announce({ id: testId, room: 'a' }));
 });
-//
-// test('0:can send message to 1', function(t) {
-//   var message = '/to|' + members[1].id + '|/hello|foo';
-//
-//   t.plan(1);
-//   members[1].once('data', function(data) {
-//     t.equal(message, data, 'ok');
-//   });
-//
-//   members[0].process(message);
-// });
-//
-// test('1:can send a message to 0', function(t) {
-//   var message = '/to|' + members[0].id + '|/ehlo|bar';
-//
-//   t.plan(1);
-//   members[0].once('data', function(data) {
-//     t.equal(message, data, 'ok');
-//   });
-//
-//   members[1].process(message);
-// });
